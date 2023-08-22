a339af0fc5690b8253d20e1e07faaee2d94000e3
Argueta <argueta03@hotmail.com>
2023-08-22 15:32:31 +0200

    App Auth

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environments';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private readonly baseUrl: string = environment.baseUrl;
+  private http = inject(HttpClient);
+
+  private _currentUser = signal<User|null>(null);
+  private _authStatus = signal<AuthStatus>();
+
+  constructor() { }
+
+  login( email:string, password:string ):Observable<boolean>{
+
+    return of(true);
+  }
+
+}
